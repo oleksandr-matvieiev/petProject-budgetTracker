9c2a50f2a5319f74b8835d1b768de5c405ac45cc
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-02-13 22:17:17 +0100

    init commit

---
+++ src/main/java/com/example/petprojectbudgettracker/PetProjectBudgetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.petprojectbudgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectBudgetTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectBudgetTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petprojectbudgettracker/PetProjectBudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petprojectbudgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectBudgetTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
