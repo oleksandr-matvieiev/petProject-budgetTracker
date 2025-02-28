892263e2ea8d2659612e28204692e52522dbf740
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-02-28 16:14:21 +0100

    Update login method to return json token

--- src/main/java/com/example/petprojectbudgettracker/controllers/AuthController.java
+++ src/main/java/com/example/petprojectbudgettracker/controllers/AuthController.java
@@ -5,6 +5,9 @@ import com.example.petprojectbudgettracker.services.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -21,7 +24,10 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody AuthRequest authRequest) {
-        return ResponseEntity.ok(authService.login(authRequest));
+    public ResponseEntity<Map<String, String>> loginUser(@RequestBody AuthRequest authRequest) {
+        String token = authService.login(authRequest);
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return ResponseEntity.ok(response);
     }
 }
